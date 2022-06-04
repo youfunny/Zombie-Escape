d3d15cb91c9c791db54694323a2a24eeee9a4484
peachysangria <colgate_family@list.ru>
2022-06-04 13:42:44 +0300

    player and camera control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,19 +4,58 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    PlayerControl player;
-    [SerializeField]
-    Vector3 offset;
+    public Camera cam;
+    public Transform target;
+    private float speedX = 480f;
+    private float speedY = 450f;
+    public float limitY = 60f;
+    public float minDistance = 1.5f;
+    public LayerMask obstacles;
+    public LayerMask noPlayer;
+
+    private float maxDistance;
+    private Vector3 localPosition;
+    private float currentYRot;
+    private Vector3 p
+    {
+        get { return transform.position; }
+        set { transform.position = value; }
+    }
 
     void Start()
     {
-        player = FindObjectOfType<PlayerControl>();
-        offset = transform.position - player.transform.position;
-
+        localPosition = target.InverseTransformPoint(p);
+        maxDistance = Vector3.Distance(p, target.position);
     }
+
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        p = target.TransformPoint(localPosition);
+        CameraRotation();
+        localPosition = target.InverseTransformPoint(p);
+    }
 
+    void CameraRotation()
+    {
+        var mx = Input.GetAxis("Mouse X");
+        var my = -Input.GetAxis("Mouse Y");
+
+        if (my != 0)
+        {
+            var tmp = Mathf.Clamp(currentYRot + my * speedY * Time.deltaTime, -limitY, limitY);
+            if (tmp != currentYRot)
+            {
+                var rot = tmp - currentYRot;
+                transform.RotateAround(target.position, transform.right, rot);
+                currentYRot = tmp;
+            }
+        }
+
+        if (mx != 0)
+        {
+            transform.RotateAround(target.position, Vector3.up, mx * speedX * Time.deltaTime);
+        }
+
+        transform.LookAt(target);
     }
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,51 +6,77 @@ using UnityEngine.AI;
 public class PlayerControl : MonoBehaviour
 {
     NavMeshAgent navMeshAgent;
-    public float speed;
-    PlayerCursor cursor;
-    [SerializeField]
-    public float sprintBoost = 1.5f;
-    public bool isBoosted = false;
-
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        //cursor = FindObjectOfType<PlayerCursor>();
-        //navMeshAgent.updateRotation = false;
+        navMeshAgent.speed = 900;
     }
+
     void Update()
     {
-        //Vector3 forward = cursor.transform.position - transform.position;
-        //transform.rotation = Quaternion.LookRotation(new Vector3(forward.x, 0, forward.z));
-        Vector3 moveDirection = Vector3.zero;
-        if (Input.GetKey(KeyCode.A))
-            moveDirection.z = -1.0f;
-        if (Input.GetKey(KeyCode.D))
-            moveDirection.z = 1.0f;
-        if (Input.GetKey(KeyCode.W))
-            moveDirection.x = -1.0f;
-        if (Input.GetKey(KeyCode.S))
-            moveDirection.x = 1.0f;
-        if (Input.GetKey(KeyCode.LeftShift))
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(horizontal, vertical);
+
+        if (direction.magnitude <= 0)
         {
-            if (!isBoosted)
-            {
-                speed *= sprintBoost;
-                isBoosted = true;
-            }
-        }
-        else
-        {
-            if (isBoosted)
-            {
-                speed /= sprintBoost;
-                isBoosted = false;
-            }
+            //для анимации
         }
 
-        navMeshAgent.velocity = moveDirection.normalized * speed;
+        if (Mathf.Abs(direction.y) > 0.1f)
+        {
+            Vector3 destination = transform.position + transform.right * direction.x + transform.forward * direction.y;
+            navMeshAgent.destination = destination;
+        }
+        else 
+        {
+            navMeshAgent.destination = transform.position;
+            transform.Rotate(0, direction.x * navMeshAgent.angularSpeed * Time.deltaTime, 0);
+        }
     }
+    //public float speed;
+    //PlayerCursor cursor;
+    //[SerializeField]
+    //public float sprintBoost = 1.5f;
+    //public bool isBoosted = false;
 
+
+    //void Start()
+    //{
+    //    navMeshAgent = GetComponent<NavMeshAgent>();
+    //    cursor = FindObjectOfType<PlayerCursor>();
+    //    navMeshAgent.updateRotation = false;
+    //}
+    //void Update()
+    //{
+    //    Vector3 forward = cursor.transform.position - transform.position;
+    //    transform.rotation = Quaternion.LookRotation(new Vector3(forward.x, 0, forward.z));
+    //    Vector3 moveDirection = Vector3.zero;
+    //    if (Input.GetKey(KeyCode.A))
+    //        moveDirection.z = -1.0f;
+    //    if (Input.GetKey(KeyCode.D))
+    //        moveDirection.z = 1.0f;
+    //    if (Input.GetKey(KeyCode.W))
+    //        moveDirection.x = -1.0f;
+    //    if (Input.GetKey(KeyCode.S))
+    //        moveDirection.x = 1.0f;
+    //    if (Input.GetKey(KeyCode.LeftShift))
+    //    {
+    //        if (!isBoosted)
+    //        {
+    //            speed *= sprintBoost;
+    //            isBoosted = true;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (isBoosted)
+    //        {
+    //            speed /= sprintBoost;
+    //            isBoosted = false;
+    //        }
+    //    }
+    //}
 }
 
