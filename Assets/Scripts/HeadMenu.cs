929ecc8931b189de8af1d0833b993dfef0cf8284
peachysangria <colgate_family@list.ru>
2022-06-09 04:55:47 +0300

    added all types of ui

--- Assets/Scripts/HeadMenu.cs
+++ Assets/Scripts/HeadMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HeadMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame() 
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame() 
     {
-        
+        Debug.Log("He left us...");
+        Application.Quit();
     }
 }
